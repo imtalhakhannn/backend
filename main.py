ca011cf224dbc004871c2791d5041cdf4052e8f0
Talha Khan <talha.khan@nets-international.com>
2024-05-26 15:15:35 +0500

    changes

--- main.py
+++ main.py
@@ -855,5 +855,10 @@ def create_car_listing():
 
     return jsonify({'message': 'Car listing created successfully'}), 201
 
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+    
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
